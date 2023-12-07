ac47e150c805c4127a44bd741d75e5df13b10683
David Barbosa <58398380+DavidBarbosa425@users.noreply.github.com>
2023-12-07 15:28:37 -0300

    titulo-form e resumo (#1)

--- src/app/features/painel-colaborador.module.ts
+++ src/app/features/painel-colaborador.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from "@angular/core";
 import { PainelColaboradorComponent } from "./painel-colaborador/pages/painel-colaborador.component";
+import { CustomFormularioModule } from "../shared/custom-formulario.module";
+import { PainelColaboradorResumoComponent } from "./painel-colaborador/pages/resumo/painel-colaborador-resumo.component";
 
 
 @NgModule({
-  declarations: [
-    PainelColaboradorComponent,
-
-  
-  ],
-  imports: [    
-
-  ],
-  providers: [
-
-  ]
+    declarations: [
+        PainelColaboradorComponent,
+        PainelColaboradorResumoComponent
+    ],
+    providers: [],
+    imports: [
+        CustomFormularioModule
+    ]
 })
 export class PainelColaboradorModule { }
